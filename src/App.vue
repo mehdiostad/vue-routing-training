ea74769f71e835cb5d766034bb5e47d6f518a2e6
mehdi <me.ostad80@gmail.com>
2022-08-09 11:14:29 +0430

    repo added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link :to="{name: 'jobs'}">Jobs</router-link> 
+    
   </nav>
   <router-view/>
+  <button @click="backward">Go Backward</button>
+  <button @click="redirect">Redirect</button>
+  <button @click="forward">Go Forward</button>
 </template>
+<script>
+export default {
+  methods:{
+    backward(){
+      this.$router.go(-1);
+    },
+    redirect(){
+      this.$router.push({name: 'home'})
+    },
+    forward(){
+      console.log('clicked')
+       this.$router.go(1);
 
+    }
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -22,9 +43,28 @@ nav {
 nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  background: crimson;;
+  text-decoration: none;
+  color: white;
+  padding: 10px;
+  border-radius: 7px;
+}
+button{
+  margin:10px;
+  padding: 10px;
+  background: #ddd;
+  border-radius: 7px;
+  border: none;
+  cursor: pointer;
+}
+button:hover{
+  background: crimson;
+  color: white;
+
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>This is Home Page!</h1>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque placeat explicabo minima ullam laudantium harum est consectetur asperiores illo ratione expedita blanditiis laboriosam, numquam at voluptatem dolores iste nulla possimus repellat iusto incidunt modi necessitatibus labore. Consequatur suscipit tempora quibusdam maiores asperiores natus ducimus voluptatum velit iure nostrum molestias similique vitae consequuntur laboriosam unde dolorem dolor enim deleniti magnam sapiente, quia porro quam placeat. Repellat reiciendis aliquid asperiores voluptatem quos recusandae, aliquam minima beatae, quo a laborum quod exercitationem consectetur totam unde soluta qui deleniti officiis eius ad expedita consequuntur. Neque eveniet, optio officiis sunt placeat quis quam error quo!</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+
   }
 }
 </script>
